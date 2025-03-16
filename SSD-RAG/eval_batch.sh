01ad975c5ab2edba750b9635f6c59100d9947a8e
Kunwoo Shin <kunwooshin97@gmail.com>
2025-03-16 23:02:26 +0900

    Delete SSD-RAG directory

--- SSD-RAG/eval_batch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-torchrun --nproc_per_node 1 --master_port=29500 eval_batch.py --db_dir=/mnt/raid0/kunwooshin/data/db --cache_dir=/mnt/raid0/kunwooshin/data/cache --query_file=./questions/query.jsonl --top_k 2 --use_past_cache=False --bsz 1 --max_new_tokens 100 --total_num 128
--- SSD-RAG/preprocessing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-torchrun --nproc_per_node 1 preprocessing.py --docs_dir=/mnt/kunwooshin/SSD-RAG/documents --db_dir=/mnt/raid0/kunwooshin/data/db --cache_dir=/mnt/raid0/kunwooshin/data/cache_K --chunk_size 1024
