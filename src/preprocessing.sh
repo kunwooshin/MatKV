de5fe2eb44fc5d5fd5011bef4aca8b9ed2c050be
Kunwoo Shin <kunwooshin97@gmail.com>
2025-03-21 13:46:59 +0900

    Update preprocessing.sh

--- src/preprocessing.sh
+++ src/preprocessing.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-torchrun --nproc_per_node 1 preprocessing.py --docs_dir=./data/TurboRAG/documents --db_dir=./data/TurboRAG/db --cache_dir=./data/TurboRAG/cache --chunk_size 1024
+torchrun --nproc_per_node 1 preprocessing.py --docs_dir=./data/TurboRAG/documents --db_dir=./data/db --cache_dir=./data/cache --chunk_size 1024
